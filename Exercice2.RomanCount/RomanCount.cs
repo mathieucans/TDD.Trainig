26e8940bd4cfc17fa5a55d38a6a37279a8b0ee10
mathieucans <mathieucans@gmail.com>
2016-06-22 22:36:05 +0200

    before refactoring

--- Exercice2.RomanCount/RomanCount.cs
+++ Exercice2.RomanCount/RomanCount.cs
@@ -6,18 +6,35 @@ namespace Exercice2.RomanCount
 	{
 		public string Convert(int arabic)
 		{
-			var result = "";
-			if (arabic > 2)
+			var result = "";		
+
+			while (arabic > 0)
 			{
-				result += "I";
-			}
-			if (arabic > 1)
-			{
-				result += "I";
-			}
-			if (arabic > 0)
-			{
-				result += "I";
+				if (arabic >= 50)
+				{
+					result += "L";
+					arabic -= 50;
+				}
+				if (arabic < 40 && arabic >= 10)
+				{
+					result += "X";
+					arabic -= 10;
+				}
+				if (arabic < 10 && arabic >= 5)
+				{
+					result += "V";
+					arabic -= 5;
+				}
+				if (arabic < 5 && arabic >= 4 )
+				{
+					result += "IV";
+					arabic -= 4;
+				}
+				if (arabic < 4 && arabic >= 1)
+				{
+					result += "I";
+					arabic--;
+				}
 			}
 
 			return result;
