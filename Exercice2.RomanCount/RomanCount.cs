a8713524d3d8ae3335f28804c02827a0c032c306
mathieucans <mathieucans@gmail.com>
2016-06-29 22:57:00 +0200

    array

--- Exercice2.RomanCount/RomanCount.cs
+++ Exercice2.RomanCount/RomanCount.cs
@@ -6,37 +6,53 @@ namespace Exercice2.RomanCount
 	{
 		public string Convert(int arabic)
 		{
-			var result = "";		
+			var result = "";
 
-			var converter = new RomanConverter(1,"I");
+			RomanConverter converter = null;
+			var converters = new[]
+			{
+				new RomanConverter(1, "I"),
+				new RomanConverter(4, "IV"),
+				new RomanConverter(5, "V"),
+				new RomanConverter(9, "IX"),
+				new RomanConverter(10, "X"),
+				new RomanConverter(50, "L"),
+			};
 
 			if (arabic > 0)
 			{
 				if (arabic >= 50)
 				{
-					result += "L" + Convert(arabic - 50);
+					converter = converters[5];
 				}
 				if (arabic < 40 && arabic >= 10)
 				{
-					result += "X" + Convert(arabic - 10);
+					converter = converters[4];
 				}
 				if (arabic < 10 && arabic >= 9)
 				{
-					result += "IX" + Convert(arabic - 9);
+					converter = converters[3];
 				}
 				if (arabic < 9 && arabic >= 5)
 				{
-					result += "V" + Convert(arabic - 5);
+					converter = converters[2];
 				}
 				if (arabic < 5 && arabic >= 4)
 				{
-					result += "IV" + Convert(arabic - 4);
+					converter = converters[1];
 				}
-				if (arabic <= 3)				
+				if (arabic < 4 && arabic >= 1)				
 				{
-					result += "I" + Convert(arabic - 1);
-				}
-				
+					converter = converters[0];
+				}				
+			}
+
+			if (converter != null)
+			{
+				result = String.Concat(
+					result,
+					converter.Roman,
+					Convert(arabic - converter.Arabic));
 			}
 
 			return result;
