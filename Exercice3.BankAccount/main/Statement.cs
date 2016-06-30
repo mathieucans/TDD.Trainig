49c36d4150bf9a2b1a4b762769a18891dc4bf6b9
mathieucans <mathieucans@gmail.com>
2016-06-30 15:34:50 +0200

    red - printlines

--- Exercice3.BankAccount/main/Statement.cs
+++ Exercice3.BankAccount/main/Statement.cs
@@ -1,6 +1,39 @@
+using System;
+using System.Collections.Generic;
+
 namespace Exercice3.BankAccount.main
 {
 	public class Statement
 	{
+		private List<StatementLine> _lines;
+
+		public Statement()
+		{
+			_lines = new List<StatementLine>();
+		}
+
+		public IEnumerable<StatementLine> Lines
+		{
+			get { return _lines; }
+		}
+
+		public void AddLine(DateTime date, double amount, int balance)
+		{
+			_lines.Add(new StatementLine(date, amount, balance));
+		}
+	}
+
+	public class StatementLine
+	{
+		public DateTime Date { get; private set; }
+		public double Amount { get; private set; }
+		public int Balance { get; private set; }
+
+		public StatementLine(DateTime date, double amount, int balance)
+		{
+			Date = date;
+			Amount = amount;
+			Balance = balance;
+		}
 	}
 }
--- Exercice3.BankAccount/test/PrintServiceShould.cs
+++ Exercice3.BankAccount/test/PrintServiceShould.cs
@@ -1,4 +1,6 @@
-﻿using Exercice3.BankAccount.main;
+﻿using System;
+using System.Text;
+using Exercice3.BankAccount.main;
 using Exercice3.BankAccountKata;
 using NFluent;
 using Xunit;
@@ -7,7 +9,7 @@ namespace Exercice3.BankAccount.test
 {
 	public class PrintServiceShould
 	{
-		private const string HEADER_LINE = "DATE|AMOUNT|BALANCE\r\n";
+		private const string HEADER_LINE = "DATE|AMOUNT|BALANCE";
 		private PrintService _printService;
 		private StringPrinterDriverAdapter _printerDriver;
 
@@ -16,14 +18,31 @@ namespace Exercice3.BankAccount.test
 			_printerDriver = new StringPrinterDriverAdapter();
 			_printService = new PrintService(_printerDriver);
 		}
-
 		
 		[Fact]
 		public void should_print_the_header_first()
 		{
 			_printService.Print(new Statement());
 
-			Check.That(_printerDriver.BuildPrintedLines()).Equals(HEADER_LINE);
+			Check.That(_printerDriver.BuildPrintedLines()).Equals(
+				new StringBuilder()
+					.AppendLine(HEADER_LINE).ToString());
+		}
+
+		[Fact]
+		public void should_print_each_Lines()
+		{
+			var statement = new Statement();
+			statement.AddLine(DateTime.Parse("30/06/2015"), -10.0, 500);
+			statement.AddLine(DateTime.Parse("20/06/2015"), 1000, 5000);
+
+			_printService.Print(statement);
+
+			Check.That(_printerDriver.BuildPrintedLines()).Equals(
+				new StringBuilder()
+					.AppendLine(HEADER_LINE)
+					.AppendLine("30/06/2015|-10.00|500.00")
+					.AppendLine("30/06/2015|1000.00|5000.00").ToString());
 		}
 	}
 }
