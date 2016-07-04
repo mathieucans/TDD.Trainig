1b700d89328b7a7b47c600f62912e2b89b6ad0e8
mathieucans <mathieucans@gmail.com>
2016-07-04 13:25:40 +0200

    Stack example

---
+++ Exercice1/MyStack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercice1
+{
+	public class MyStack
+	{
+		private Stack<object> _stack;
+
+		public MyStack()
+		{
+			_stack = new Stack<object>();
+		}
+
+		public object Pop()
+		{
+			if (IsEmpty()) throw new MyException();
+
+			return _stack.Pop();
+		}
+
+		public void Push(object anObject)
+		{
+			_stack.Push(anObject);
+		}
+
+		private bool IsEmpty()
+		{
+			return _stack.Count == 0;
+		}
+	}
+
+	public class MyException : Exception
+	{
+	}
+}
---
+++ Exercice1/StackShould.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NFluent;
+using Xunit;
+
+namespace Exercice1
+{
+    public class StackShould
+    {
+	    private readonly object AN_OBJECT;
+
+
+	    public StackShould()
+	    {
+		    AN_OBJECT = new object();
+	    }
+
+	    [Fact]
+	    public void pop_the_last_pushed_object()
+	    {
+		    var stack = new MyStack();
+
+		    stack.Push(AN_OBJECT);
+
+		    Check.That(stack.Pop()).Equals(AN_OBJECT);
+	    }
+
+	    [Fact]
+	    public void throw_an_exception_if_popped_when_empty()
+	    {
+			var stack = new MyStack();
+			stack.Push(AN_OBJECT);
+		    stack.Pop();
+
+		    Check.That(() => stack.Pop()).Throws<MyException>();
+	    }
+    }
+
+	
+}
